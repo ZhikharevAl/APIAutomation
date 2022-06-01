ad150ce00e600e15e435c7a4e8047788777f3c2d
AlexeyZh <waltafunk@gmail.com>
2022-06-01 17:28:16 +0300

    add display response content

--- GET_Requests/FetchUserData.py
+++ GET_Requests/FetchUserData.py
@@ -2,5 +2,13 @@ import requests
 
 # API URL
 
-url = "https://reqres.in//api/users?page=2"
+url = "https://reqres.in/api/users?page=2"
 
+# Send Get Request
+
+response = requests.get(url)
+
+
+# Display Response Content
+
+print(response.content)
