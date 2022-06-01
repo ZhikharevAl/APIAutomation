075dca47fde9bac4b6b70b45f4ca5d92bdbbc83d
AlexeyZh <waltafunk@gmail.com>
2022-06-01 18:16:10 +0300

    add Fetch Value using Json Path

--- GET_Requests/FetchUserData.py
+++ GET_Requests/FetchUserData.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import jsonpath
 
 # API URL
 
@@ -8,7 +10,20 @@ url = "https://reqres.in/api/users?page=2"
 
 response = requests.get(url)
 
-
 # Display Response Content
 
 print(response.content)
+print(response.headers)
+
+# Parse response to Json format
+
+json_response = json.loads(response.text)
+print(json_response)
+
+# Fetch Value using Json Path
+
+pages = jsonpath.jsonpath(json_response, 'total_pages')
+print(pages[0])
+
+pages = jsonpath.jsonpath(json_response, 'total_pages')
+assert pages[0] == 5
