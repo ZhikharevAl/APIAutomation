f2ad933c5d13faaf74a2e04b05cf0da9491e7dcd
AlexeyZh <waltafunk@gmail.com>
2022-06-01 18:33:39 +0300

    add DeleteUser.py

---
+++ GET_Requests/DeleteUser.py
@@ -0,0 +1,13 @@
+import requests
+
+# API URL
+
+url = "https://reqres.in/api/users/2"
+
+response = requests.delete(url)
+
+# Fetch Response Code
+
+print(response.status_code)
+
+assert response.status_code == 204
